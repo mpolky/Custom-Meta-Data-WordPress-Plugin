0e6521c92aa4af6e7273ab3fb1d3465125810bad
Matt Polky <matthew.polky@guaranteedrate.com>
2015-11-02 15:54:09 -0600

    moving around init to register custom post type

--- custom-meta-data.php
+++ custom-meta-data.php
@@ -6,7 +6,48 @@ Version: 1.0
 Author: Matt Polky
 License: GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
-*/
+*/ 
+function cptui_register_my_cpts() {
+	$labels = array(
+		"name" => "GRPosts",
+		"singular_name" => "GRPost",
+		"menu_name" => "My Posts",
+		"all_items" => "All Posts",
+		"add_new" => "Add New",
+		"add_new_item" => "Add New Post",
+		"edit" => "Edit",
+		"edit_item" => "Edit Post",
+		"new_item" => "New Post",
+		"view" => "View",
+		"view_item" => "View Post",
+		"search_items" => "Search Post",
+		"not_found" => "No Posts found",
+		"not_found_in_trash" => "No Posts found in Trash",
+		"parent" => "Parent Post",
+		);
+
+	$args = array(
+		"labels" => $labels,
+		"description" => "Custom GRPost Type",
+		"public" => true,
+		"show_ui" => true,
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => array( "slug" => "grpost", "with_front" => true ),
+		"query_var" => true,
+						
+		"taxonomies" => array( "category", "post_tag" )
+	);
+	register_post_type( "grpost", $args );
+
+	// End of cptui_register_my_cpts()
+}
+add_action('init', 'cptui_register_my_cpts');
+
 function customMetaData_plugin_init()
 {
 	global $customMetaData_plugin;
@@ -18,8 +59,6 @@ function customMetaData_plugin_init()
 	add_filter('json_prepare_attachment', array( $customMetaData_plugin, 'add_custom_fields_meta' ), 10, 3);
 	add_filter('json_prepare_term', array( $customMetaData_plugin, 'add_custom_taxonomies_meta' ), 10, 2);
 	add_filter('json_prepare_user', array( $customMetaData_plugin, 'add_custom_users_meta' ), 10, 2);
-
-	add_action('init', array($customMetaData_plugin, 'cptui_register_my_cpts') );
 }
 add_action('wp_json_server_before_serve', 'customMetaData_plugin_init');
 class CustomMetaData_plugin {
@@ -75,47 +114,5 @@ class CustomMetaData_plugin {
 		$valid_vars = array_merge( $valid_vars, array( 'meta_key', 'meta_value' ) );
 		return $valid_vars;
 	}
-
-	public function cptui_register_my_cpts() {
-	$labels = array(
-		"name" => "GRPosts",
-		"singular_name" => "GRPost",
-		"menu_name" => "My Posts",
-		"all_items" => "All Posts",
-		"add_new" => "Add New",
-		"add_new_item" => "Add New Post",
-		"edit" => "Edit",
-		"edit_item" => "Edit Post",
-		"new_item" => "New Post",
-		"view" => "View",
-		"view_item" => "View Post",
-		"search_items" => "Search Post",
-		"not_found" => "No Posts found",
-		"not_found_in_trash" => "No Posts found in Trash",
-		"parent" => "Parent Post",
-		);
-
-	$args = array(
-		"labels" => $labels,
-		"description" => "Custom GRPost Type",
-		"public" => true,
-		"show_ui" => true,
-		"has_archive" => true,
-		"show_in_menu" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "grpost", "with_front" => true ),
-		"query_var" => true,
-						
-		"taxonomies" => array( "category", "post_tag" )
-	);
-	register_post_type( "grpost", $args );
-
-	// End of cptui_register_my_cpts()
-	}
-
-
 }
 ?>
